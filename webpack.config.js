6d57a329b52f7c11f839de1feeb7c4d425713976
Patty Long <pattygxlong@gmail.com>
2023-01-18 20:36:36 -0800

    Move index.html and get rid of dev server in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,5 @@ module.exports = {
     publicPath: "/public/dist/",
     filename: "bundle.js"
   },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
   plugins: [new webpack.HotModuleReplacementPlugin()]
 };
