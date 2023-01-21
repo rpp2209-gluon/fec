34e7018e86f3ceb805a010ea502c7a09fc750ba0
achaidez <chaidez62@gmail.com>
2023-01-21 13:47:33 -0700

    Set up server get requests for reviews and products

--- server/server.js
+++ server/server.js
@@ -1,9 +1,85 @@
 const path = require("path");
 const express = require("express"); // npm installed
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const config = require("../config.js");
+// console.log(API_KEY)
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../public/dist")));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 // other configuration...
 
 app.listen(3000);
+
+// PRODUCTS API
+
+app.get('/products', (req, res) => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products', {
+      headers: {
+        'Authorization': `${config.API_KEY}`
+        }
+      })
+      .then((response) => {
+        console.log('GET products returned: ')
+        console.log(response.data);
+        res.status(200).send(response.data);
+      })
+      .catch((err) => {
+        console.log('GET products errored: ');
+        console.log(err);
+        res.status(500).send(err);
+      })
+  });
+
+
+
+
+
+// REVIEWS API
+
+app.get('/reviews/:id', (req, res) => {
+    console.log(req.params.id);
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews?product_id=' + req.params.id, {
+      headers: {
+        'Authorization': `${config.API_KEY}`
+        }
+      })
+      .then((response) => {
+        console.log('GET reviews returned: ')
+        console.log(response.data);
+        res.status(200).send(response.data);
+      })
+      .catch((err) => {
+        console.log('GET reviews errored: ');
+        console.log(err);
+        res.status(500).send(err);
+      })
+});
+
+
+
+
+
+
+
+
+// Questions and Answers API
+
+
+
+
+
+// CART APIs
+
+
+
+
+
+// INTERACTIONS APIs
+
+
+
