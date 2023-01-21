7de0a87953bcfa3577c1f4eaac8b4bcffd4913c7
Kevin Hoang <knhoangre@gmail.com>
2023-01-21 16:20:56 -0500

    Completed most of question, answer, and search. Need to incorporate question modal better.

---
+++ public/src/Components/QuestionAnswer/Question.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import Answer from './Answer.jsx';
+
+const sortAnswersByHelpfulness = (data) => {
+  const sellerData = [];
+  const otherData = [];
+  for (var i in data) {
+    if (data[i].answerer_name.toLowerCase() === 'seller') {
+      sellerData.push(data[i]);
+    } else {
+      otherData.push(data[i]);
+    }
+  }
+  sellerData.sort((a, b) => b.helpfulness - a.helpfulness);
+  otherData.sort((a, b) => b.helpfulness - a.helpfulness);
+  return sellerData.concat(otherData);
+};
+
+var Question = (props) => {
+  const [numberDisplayAnswers, setNumberDisplayAnswers] = useState(Object.keys(props.questionData.answers).length > 1 ? 2 : Object.keys(props.questionData.answers).length);
+  const [helpfulness, setHelpfulness] = useState({ click: false, helpfulness: props.questionData.question_helpfulness });
+
+
+  var displayAnswer = () => {
+    var resultArr = [];
+    const sortedAnswersArr = sortAnswersByHelpfulness(props.questionData.answers);
+    for (var i = 0; i < numberDisplayAnswers; i++) {
+      resultArr.push(<Answer key={sortedAnswersArr[i].id} answerData={sortedAnswersArr[i]} />)
+    }
+    return resultArr;
+  };
+
+  const handleLoadMoreAnswers = () => {
+    setNumberDisplayAnswers(Object.keys(props.questionData.answers).length);
+  };
+
+  const handleCollapseAnswers = () => {
+    setNumberDisplayAnswers(2);
+  };
+
+  const showMoreAnswers = () => {
+    if (numberDisplayAnswers < 2) {
+      return null;
+    } else if (numberDisplayAnswers !== Object.keys(props.questionData.answers).length) {
+      return <p className='see-more-answers' onClick={handleLoadMoreAnswers}>SEE MORE ANSWERS</p>
+    } else {
+      return <p className='collapse-answers' onClick={handleCollapseAnswers}>COLLAPSE ANSWERS</p>
+    }
+  };
+
+  const handleClickHelpfulness = () => {
+    if (helpfulness.click) {
+      var newHelpfulness = { click: false, helpfulness: helpfulness.helpfulness - 1 };
+      setHelpfulness(newHelpfulness);
+    } else {
+      var newHelpfulness = { click: true, helpfulness: helpfulness.helpfulness + 1 };
+      setHelpfulness(newHelpfulness);
+    }
+  };
+
+  const handleAddAnswerClick = () => {
+
+  }
+
+  return (
+    <div>
+      <p id='question'>Q: {props.questionData.question_body}</p>
+      <p onClick={handleClickHelpfulness}>Helpful? Yes ({helpfulness.helpfulness})</p>
+      {displayAnswer()}
+      {showMoreAnswers()}
+      <p id='add-answer' onClick={handleAddAnswerClick}>ADD ANSWER</p>
+    </div>
+  )
+}
+
+export default Question;
