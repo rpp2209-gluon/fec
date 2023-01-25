4252a7a96cc5677527f2c587fc3c6c8e845eb5c1
Patty Long <pattygxlong@gmail.com>
2023-01-24 21:14:43 -0800

    Rendering basic reviews with information, including checkmark and show all button logic

--- public/src/Components/ratings_components/reviewTile.jsx
+++ public/src/Components/ratings_components/reviewTile.jsx
@@ -1,35 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 var ReviewTile = (props) => {
 
-  // "review_id": 5,
-  // "rating": 3,
-  // "summary": "I'm enjoying wearing these shades",
-  // "recommend": false,
-  // "response": null,
-  // "body": "Comfortable and practical.",
-  // "date": "2019-04-14T00:00:00.000Z",
-  // "reviewer_name": "shortandsweeet",
-  // "helpfulness": 5,
-  // "photos": [{
-  //     "id": 1,
-  //     "url": "urlplaceholder/review_5_photo_number_1.jpg"
-  //   },
-  //   {
-  //     "id": 2,
-  //     "url": "urlplaceholder/review_5_photo_number_2.jpg"
-  //   },
+  const [showAllReview, setShowAllReview] = useState(false);
+
+  const reviewClickHandler = () => {
+    console.log('i was clicked');
+    setShowAllReview(!showAllReview);
+  }
 
   return (
     <>
-      <h2>Review ID</h2>
-      <p>{props.reviewData.review_id}</p>
-      <h2>Rating</h2>
-      <p>{props.reviewData.rating}</p>
-      <h2>Summary</h2>
-      <p>{props.reviewData.summary}</p>
-      <h2>Response</h2>
-      <p>{props.reviewData.response}</p>
+      <h2>Review</h2>
+      <p>Review ID: {props.reviewData.review_id}</p>
+      <p>Review Date: {moment(props.reviewData.date).format('MMMM Do, YYYY')}</p>
+      <p>Reviewer Name: {props.reviewData.reviewer_name}</p>
+      <p>Rating: {props.reviewData.rating}</p>
+      <p><b>{props.reviewData.summary}</b></p>
+      <p>Review Body: {showAllReview ? props.reviewData.body.slice(0, 250) : props.reviewData.body}</p>
+      { props.reviewData.body.length >= 250 ?
+        <button onClick={reviewClickHandler}>{showAllReview ? 'Collapse' : 'Show More'}</button> : <></>
+      }
+      <p>Response: {props.reviewData.response}</p>
+      <p>Review Helpfulness: {props.reviewData.response}</p>
+      <p>{props.reviewData.recommend ? 'I recommend this product ✅' : ''}</p>
     </>
   )
 
