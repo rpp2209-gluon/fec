61779c2357b722f469bbdaa071bd4ccc6840c34f
Patty Long <pattygxlong@gmail.com>
2023-01-27 21:37:03 -0800

    Clean up review body and add seller response component

--- public/src/Components/ratings_components/reviewTile.jsx
+++ public/src/Components/ratings_components/reviewTile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
+import SellerResponse from './sellerResponse.jsx';
 
 var ReviewTile = (props) => {
 
@@ -13,19 +14,17 @@ var ReviewTile = (props) => {
 
   return (
     <>
-      <h2>Review</h2>
-      <p>Review ID: {props.reviewData.review_id}</p>
-      <p>Review Date: {moment(props.reviewData.date).format('MMMM Do, YYYY')}</p>
-      <p>Reviewer Name: {props.reviewData.reviewer_name}</p>
-      <p>Rating: {props.reviewData.rating}</p>
+      <p>Star Rating: {props.reviewData.rating}</p>
+      <p>{props.reviewData.reviewer_name} , {moment(props.reviewData.date).format('MMMM Do, YYYY')}</p>
       <p><b>{props.reviewData.summary}</b></p>
       <p>Review Body: {showAllReview ? props.reviewData.body.slice(0, 250) : props.reviewData.body}</p>
       { props.reviewData.body.length >= 250 ?
         <button onClick={reviewClickHandler}>{showAllReview ? 'Collapse' : 'Show More'}</button> : <></>
       }
-      <p>Response: {props.reviewData.response}</p>
-      <p>Review Helpfulness: {props.reviewData.response}</p>
+      <p> {props.reviewData.response ? <SellerResponse response={props.reviewData.repsonse}/> : <></> }</p>
+      <p>Was This Review Helpful? Yes ({props.reviewData.helpfulness})</p>
       <p>{props.reviewData.recommend ? 'I recommend this product ✅' : ''}</p>
+      <b>================================================================</b>
     </>
   )
 
