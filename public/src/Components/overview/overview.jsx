7bc39f5ed21911f8f848a6ce4331076470c17138
XQ2013 <xiqiaolearner@gmail.com>
2023-01-28 12:25:20 -0600

    Add server method

--- public/src/Components/overview/overview.jsx
+++ public/src/Components/overview/overview.jsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom/client';
+import axios from "axios";
 
-function OverView () {
-  return (
+const OverView = (props) => {
+
+  const [id, setId] = useState(71697);
+  const [product, setProduct] = useState({});
+  const [style, setStyle] = useState([]);
+  const [related, setRelated] = useState([]);
+
+  useEffect(() => {
+    console.log('id:, ', id);
+    axios.get('/products/:product_id', {
+      params: {
+        id: String(id),
+      }
+    })
+      .then((data) => {
+        setProduct(data.data);
+      })
+      .then(() => {
+        axios.get('/products/:product_id/styles', {
+          params: {
+            id: String(id),
+          }
+        })
+          .then((data) => {
+            setStyle(data.data);
+          })
+      })
+      .then(() => {
+        axios.get('/products/:product_id/related', {
+          params: {
+            id: String(id),
+          }
+        })
+          .then((data) => {
+            setRelated(data.data);
+          })
+      });
+  }, []);
+
+
+
+
+
+
+
+
+  return (<div>
     <h1>OverView Section</h1>
+    <div>1. Product Information</div>
+    <div>2. Style Selector </div>
+    <div> 3. add to cart  </div>
+    <div>4. imagine</div>
+
+  </div>
+
+
   )
 };
 
