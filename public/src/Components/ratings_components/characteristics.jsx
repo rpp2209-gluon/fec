32731a91342d4a15b7d4c278c58fd67a8df6a23f
Patty Long <pattygxlong@gmail.com>
2023-02-06 20:05:12 -0800

    Fixed post request on reviews

--- public/src/Components/ratings_components/characteristics.jsx
+++ public/src/Components/ratings_components/characteristics.jsx
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 var characteristics = (props) => {
 
   var labels = ['Size', 'Width', 'Comfort', 'Quality', 'Length', 'Fit'];
+  var ids = ['240595', '240596', '240597', '240598', '240599', '240600']
   var characteristics = [['A size too small', '1/2 a size too small', 'Perfect', '1/2 a size too big', 'A size too wide'],
     ['Too narrow', 'Slightly narrow', 'Perfect', 'Slightly wide' , 'Too wide'],
     ['Uncomfortable', 'Slightly uncomfortable', 'Ok', 'Comfortable', 'Perfect'],
@@ -12,10 +13,10 @@ var characteristics = (props) => {
     ['Runs tight', 'Runs slightly tight', 'Perfect', 'Runs slightly long', 'Runs long']]
 
   const onChange = (event) => {
-    var char = event.target.id.substring(5, 6);
-    var selection = event.target.id.substring(7, 8);
+    var char = event.target.id.substring(5, 11);
+    var selection = event.target.id.substring(12, 13);
     var charObj = {...props.characteristics};
-    charObj[char] = selection;
+    charObj[char] = parseInt(selection);
     props.updateCharacteristics(charObj);
   }
 
@@ -29,12 +30,12 @@ var characteristics = (props) => {
           <br></br>
           {characteristics[i].map((c, j) => (
             <Form.Check
-              key={`char-${i}-${j}-${c}`}
+              key={`char-${ids[i]}-${j}-${c}`}
               inline
               label={c}
-              name={`char-${i}-${label}`}
+              name={`char-${ids[i]}-${label}`}
               type="radio"
-              id={`char-${i}-${j}-${c}`}
+              id={`char-${ids[i]}-${j}-${c}`}
             />
           ))}
         </div>
